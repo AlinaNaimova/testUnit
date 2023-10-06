a09d86347918fcffc4878c8e8b155f94a4573e58
Alina N <my_work_space@bk.ru>
2023-10-07 01:30:17 +0300

    home task 1 tests for calculator and shop

--- src/main/java/task4/Calculator.java
+++ src/main/java/task4/Calculator.java
@@ -16,4 +16,13 @@ public class Calculator {
     }
 
     // ... другие методы, которые вы хотите протестировать
+
+    public double calculateDiscount(double purchaseAmount, double discountPercentage) {
+        if (purchaseAmount < 0 || discountPercentage < 0 || discountPercentage > 100) {
+            throw new ArithmeticException("Invalid arguments");
+        }
+        double discountAmount = purchaseAmount * (discountPercentage / 100);
+        return purchaseAmount - discountAmount;
+    }
+
 }
