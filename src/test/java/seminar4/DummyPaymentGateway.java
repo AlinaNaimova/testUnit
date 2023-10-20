371577b455cf9bf567f55d6867a4be672c555c1c
Alina N <my_work_space@bk.ru>
2023-10-20 22:07:17 +0300

    add seminar 4 and 5

---
+++ src/test/java/seminar4/DummyPaymentGateway.java
@@ -0,0 +1,9 @@
+package seminar4;
+
+public class DummyPaymentGateway implements PaymentGateway {
+
+    @Override public boolean processPayment(double amount){
+        return true;
+    // Не выполняем никаких действий, просто фиктивный объект return true;
+    }
+}
